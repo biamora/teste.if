7e8d2b143cd75111c34baf34c4bb94c8266b8c49
aluno <aluno@Lab-20>
2020-03-05 10:29:36 -0300

    trabalho3

--- script-for.sh
+++ script-for.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+#este script é para conversao de imagens
+#script for
+#
+#autor:clecya ariaty
+
+
 cd /home/aluno/Downloads/imagens-livros
 for imagem in *.jpg
 do
 	echo $imagem
 	img_sem_ext=$(ls $imagem | awk -F. '{print $1}')
 	convert $imagem  $img_sem_ext.png
+
 done
+
